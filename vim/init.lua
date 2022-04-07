801993bff3c565b0b5c02ef1cff7554fd99f98e9
fishbupt <fishbupt@gmail.com>
2022-04-07 11:31:42 +0800

    Update init.lua

--- vim/init.lua
+++ vim/init.lua
@@ -1,5 +1,5 @@
-require("options")
-require("pack")
-require("plugins")
-require("keymaps")
-require("filetype")
+require("options")      -- 通用配置
+require("pack")         -- 包管理器
+require("plugins")      -- 插件
+require("keymaps")      -- 键盘映射
+require("filetype")     -- 文件类型配置
