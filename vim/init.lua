5d64f6a1f5b30eaba388f555517573f2b6551dfe
fishbupt <fishbupt@gmail.com>
2022-04-07 17:29:37 +0800

    Update init.lua

--- vim/init.lua
+++ vim/init.lua
@@ -1,3 +1,14 @@
+function _G.put(...)
+  local objects = {}
+  for i = 1, select('#', ...) do
+    local v = select(i, ...)
+    table.insert(objects, vim.inspect(v))
+  end
+
+  print(table.concat(objects, '\n'))
+  return ...
+end
+
 require("options")      -- 通用配置
 require("pack")         -- 包管理器
 require("plugins")      -- 插件
